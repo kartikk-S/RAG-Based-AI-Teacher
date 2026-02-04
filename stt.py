295e24f8c9d18d8cc2dc0f6f8a874172f430df2b
Kartik <2k22.cse.32801@gmail.com>
2026-02-04 16:47:30 +0530

    sampling the data and running whisper checks

--- stt.py
+++ stt.py
@@ -2,6 +2,6 @@ import whisper
 
 model=whisper.load_model("large-v2")
 
-result=model.transcribe(audio="audios/1.mp3", language="hi", task="translate")
+result=model.transcribe(audio="audios/1_output.mp3", language="hi", task="translate", word_timestamps=False)
 
 print(result["text"])
