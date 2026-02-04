83010a84c90c5a8ac94c51f623aabf6a191d8a87
Kartik <2k22.cse.32801@gmail.com>
2026-02-05 00:44:26 +0530

    adding .gitignore and making changes in chunking process

---
+++ create_chunks.py
@@ -0,0 +1,28 @@
+import whisper
+import json
+import os
+
+model=whisper.load_model("large-v2")
+
+audios=os.listdir("audios")
+
+for audio in audios:
+    if "_" not in audio:
+        continue
+
+    number=audio.split("_")[0]
+    title=audio.split("_")[1][:-4]
+    print(number,title)
+    
+    # result=model.transcribe(audio=f"audios/{audios}.mp3", language="hi", task="translate", word_timestamps=False)
+    result=model.transcribe(audio=f"audios/sample.mp3", language="hi", task="translate", word_timestamps=False)
+
+    chunks=[]
+
+    for segment in result["segments"]:
+        chunks.append({"number":number, "title":title, "start":segment["start"], "end":segment["end"], "text":segment["text"]})
+
+    chunks_with_meta={"chunks":chunks, "text": result["text"]}
+
+    with open("output.json", "w") as f:
+        json.dump(chunks_with_meta,f)
--- stt.py
+++ stt.py
@@ -1,7 +1,17 @@
 import whisper
+import json
 
 model=whisper.load_model("large-v2")
 
 result=model.transcribe(audio="audios/1_output.mp3", language="hi", task="translate", word_timestamps=False)
 
-print(result["text"])
+
+chunks=[]
+
+for segment in result["segments"]:
+    chunks.append({"start":segment["start"], "end":segment["end"], "text":segment["text"]})
+
+print(chunks)
+
+with open("output.json", "w") as f:
+    json.dump(chunks,f)
